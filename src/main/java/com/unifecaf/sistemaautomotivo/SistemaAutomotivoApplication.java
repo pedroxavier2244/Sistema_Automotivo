46f0a493b5253bd6cdb494c300d062d762dfd8e2
Pedro Azara <pedro.azara09@gmail.com>
2026-06-13 23:47:32 -0300

    chore: scaffold projeto spring boot

---
+++ src/main/java/com/unifecaf/sistemaautomotivo/SistemaAutomotivoApplication.java
@@ -0,0 +1,13 @@
+package com.unifecaf.sistemaautomotivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAutomotivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAutomotivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifecaf/sistemaautomotivo/SistemaAutomotivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifecaf.sistemaautomotivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAutomotivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
